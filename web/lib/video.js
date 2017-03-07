74a030f979c1189907f38efc4b3efbfeffed6ce6
pei <epsonavy@yahoo.com>
2017-03-06 18:40:16 -0800

    fixed video player issue

--- web/lib/video.js
+++ web/lib/video.js
@@ -1,7 +1,14 @@
+/**
+ * Manages uploading and streaming of video files.
+ *
+ * @module video
+ */
+'use strict';
+
 var fs, uploadPath, supportedTypes;
- 
+
 fs = require('fs');
-uploadPath = __dirname + '/../videos';
+uploadPath = __dirname + '/../public/videos';
 supportedTypes = [
     'video/mp4',
     'video/webm',
@@ -9,39 +16,71 @@ supportedTypes = [
     'video/quicktime'
 ];
 
-function list(stream, meta)  {
-    fs.readdir(uploadPath, function (err, files) {
-        stream.write({ files : files });
+module.exports = {
+    list    : list,
+    request : request,
+    upload  : upload
+};
+
+_checkUploadDir();
+
+function _checkUploadDir(cb) {
+    cb = cb || function () {};
+
+    fs.stat(uploadPath, function (err, stats) {
+        if (
+            (err && err.errno === 34) ||
+            !stats.isDirectory()
+           ) {
+            // if there's no error, it means it's not a directory - remove it
+            if (!err) {
+                fs.unlinkSync(uploadPath);
+            }
+
+            // create directory
+            fs.mkdir(uploadPath, cb);
+            return;
+        }
+
+        cb();
     });
 }
 
+/**
+ */
+function list(stream, meta)  {
+    _checkUploadDir(function () {
+        fs.readdir(uploadPath, function (err, files) {
+            stream.write({ files : files });
+        });
+    });
+}
+
+/**
+ */
 function request(client, meta) {
     var file = fs.createReadStream(uploadPath + '/' + meta.name);
- 
+
     client.send(file);
 }
 
+/**
+ */
 function upload(stream, meta) {
     if (!~supportedTypes.indexOf(meta.type)) {
         stream.write({ err: 'Unsupported type: ' + meta.type });
         stream.end();
         return;
     }
- 
+
     var file = fs.createWriteStream(uploadPath + '/' + meta.name);
     stream.pipe(file);
- 
+
     stream.on('data', function (data) {
-        stream.write({ rx: data.length / meta.size });
+        stream.write({ rx : data.length / meta.size });
     });
- 
+
     stream.on('end', function () {
         stream.write({ end: true });
     });
 }
- 
-module.exports = {
-    list    : list,
-    request : request,
-    upload  : upload
-};
