baaffa8fcc2d93b70f0c34c4ae370cd38e4de811
root <epsonavy@yahoo.com>
2017-04-12 08:59:21 +0000

    created uservideos table

--- web/routes/upload.js
+++ web/routes/upload.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({ uploadDir: './public/videos/' });
-
 var router = express.Router();
 var config = require('../config.js');
 var fs = require('fs');
+var db = require('./db.js');
 //var Upload = require('../models/upload.js');
 
 router.post('/', multipartMiddleware, function(req, res) {
 	var files = req.files;
-	console.log(files.file);
+	//console.log(files);
+	console.log(files.upload.path);
+	console.log(req.body.username);
+
+	db.none("INSERT INTO user_videos(path, username) values($1, $2)", [files.upload.path, req.body.username])
+            .then(data => {
+              console.log("Inserted new video!");
+			  res.redirect('/dashboard');
+            })
+            .catch(error => {
+                // error;
+                console.error(error);
+            });
+
 	res.redirect('/dashboard');
 
 /*
