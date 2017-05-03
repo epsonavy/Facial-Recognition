73aa4f5484bf48b9afcd83f6920becb010437ef2
DrightenSlayer <mbinning95e@gmail.com>
2017-05-03 05:20:56 -0700

    So I spent all night thus far combining the files. I added massive amounts of time checking and added support for the URI image format as well as regular (passed by -uri of course). I also added a (-r) realtime flag, so it knows not to try to detect pupils. I refactored a good amount of the code so it makes much more sense. The pupils now work much more stably, and they have two different colors because why not.

--- pipeline/Old/Faceline.py
+++ pipeline/Old/Faceline.py
@@ -1,13 +1,10 @@
 import sys          # For argument handling
-import datetime
-from datetime import datetime
 import os, inspect  # For path handling, file deletion
 import dlib         # For facial recognition (neural network)
 import glob         # For file path handling
 import subprocess   # For running FFMPEG
 import cv2          # image input/output
 import numpy as np  # For array handling, line scaling
-#import scipy.misc
 
 '''
 TO HOOK INTO THE DATABASE (POSTGRES)
@@ -19,19 +16,14 @@ TO HOOK INTO THE DATABASE (POSTGRES)
 
 '''
 To-Do:
-Parallel Processing
 Head Pos: Yaw, Pitch, Roll
 write eye tracking coords to db
 '''
-lazy_output_file = None
-global_script_start = datetime.now() 
-URI_Handling = False
+
 def displayHelp():
     print(" Available Arguments\t\t\tDefault Arguments")
     print(" -h\tAccess the help menu (--help)")
-    print(" -uri\thandle URI images (thanks Kevin/Pei)")
     print(" -l\tlose original frames(overwrite)\tFalse")
-    print(" -r\trealtime (don't track pupils)\tFalse")
     print(" -p\tPath of the Dlib predictor\t" + run_dir + "/shape_predictor_68_face_landmarks.dat")
     print(" -f\tProcess only a single frame\tFalse")
     print(" -ss\tStart time of input file\t00:00.00")
@@ -45,81 +37,54 @@ def displayHelp():
 def in_rect(r, p):
     return p[0] > r[0] and p[1] > r[1] and p[0] < r[2] and p[1] < r[3] 
 
-def data_uri_to_cv2_img(uri):
-    encoded_data = uri.split(',')[1]
-    nparr = np.fromstring(encoded_data.decode('base64'), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
-def markFrame(img, ptsList, breadthList, wait_at_frame):
+def markFrame(img, ptsList, wait_at_frame):
     if(not wait_at_frame) or raw_input("Overwrite " + fn + " Y/N?") == "Y":
-	print("\t\tBeginning Delauney drawing algorithm")
-	tstart = datetime.now()
-	i = 0
-        for pts in ptsList:
+        for i, pts in enumerate(ptsList):
+	    if i & 1 == 0:
+		ptsArray = np.array(pts, np.int32).reshape((-1, 1, 2))
+            	bounds = (0, 0, img.shape[1], img.shape[0])
+            	subdiv = cv2.Subdiv2D(bounds)
+            	for p in pts:
+                    subdiv.insert(p)
+            	    tris = subdiv.getTriangleList();
+                for t in tris:
+                    pt1 = (t[0], t[1])
+                    pt2 = (t[2], t[3])
+                    pt3 = (t[4], t[5])
 
-	    bounds = (0, 0, img.shape[1], img.shape[0])
-            subdiv = cv2.Subdiv2D(bounds)
-            for p in pts:
-                subdiv.insert(p)
-            tris = subdiv.getTriangleList();
-            for t in tris:
-                pt1 = (t[0], t[1])
-                pt2 = (t[2], t[3])
-                pt3 = (t[4], t[5])
-
-                if in_rect(bounds, pt1) and in_rect(bounds, pt2) and in_rect(bounds, pt3):
-		    cv2.line(img, pt1, pt2, (0, 255, 0), int(breadthList[i] * 1/100), 8, 0) #tried using cv2.CV_AA
-                    cv2.line(img, pt2, pt3, (0, 255, 0), int(breadthList[i] * 1/100), 8, 0)
-                    cv2.line(img, pt3, pt1, (0, 255, 0), int(breadthList[i] * 1/100), 8, 0)
-	    i+=1
-        tend = datetime.now()
-	print("\t\t" + str(tend - tstart))
+                    if in_rect(bounds, pt1) and in_rect(bounds, pt2) and in_rect(bounds, pt3):
+			cv2.line(img, pt1, pt2, (0, 255, 0), int(np.sqrt(ptsList[i+1][0] ** 2 + ptsList[i+1][1] ** 2) * 1/100), 8, 0) #tried using cv2.CV_AA
+                        cv2.line(img, pt2, pt3, (0, 255, 0), int(np.sqrt(ptsList[i+1][0] ** 2 + ptsList[i+1][1] ** 2) * 1/100), 8, 0)
+                        cv2.line(img, pt3, pt1, (0, 255, 0), int(np.sqrt(ptsList[i+1][0] ** 2 + ptsList[i+1][1] ** 2) * 1/100), 8, 0)
     else:
         print("Permission denied.")
 
-def markPupil(img, pupilData, breadthList, wait_at_frame):
-    print("\t\tDrawing pupils to image.")
-    if (not wait_at_frame) or raw_input("Overwrite " + fn + " Y/N?") == "Y":
-        tstart = datetime.now()
-        i = 0
-	for pupil in pupilData:
-	    if pupil != (-1, -1):
-	        pupilArray = np.array(pupil, np.int32).reshape((-1, 1, 2))
-	        cv2.polylines(img, pupilArray, True, (0, 0, 255/(i+1)), int(breadthList[i/2] * 3/100))
-                cv2.polylines(img, pupilArray, True, (255/(i+1), 0, 0), int(breadthList[i/2] * 1/100))
-	    i += 1
-        tend = datetime.now()
-	print("\t\t" + str(tend - tstart))
+def markPupil(img, ptsList, wait_at_frame):
+    if(not wait_at_frame) or raw_input("Overwrite " + fn + " Y/N?") == "Y":
+        for i, pts in enumerate(ptsList):
+            if i & 1 == 0:
+	        ptsArray = np.array(pts, np.int32).reshape((-1, 1, 2))
+		cv2.polylines(img, ptsArray, True, (0, 0, 255), int(np.sqrt(ptsList[i+1][0] ** 2 + ptsList[i+1][1] ** 2) * 3/100))
+                cv2.polylines(img, ptsArray, True, (255, 0, 0), int(np.sqrt(ptsList[i+1][0] ** 2 + ptsList[i+1][1] ** 2) * 1/100))
     else:
 	print("Permission denied.")
 
 
 def detectFrame(img, detector, predictor): # returns a list of lists of points
-    print("\t\tBeginning detection")
-    tstart = datetime.now()
-    detections = detector(img, 1)# type(detections) == dlib.rectangles
-    tend = datetime.now()
-    print("\t\t" + str(tend - tstart))
-    
     ptsList = []
-    breadthList = []
-
+    print("Beginning detection.")
+    detections = detector(img, 1)# type(detections) == dlib.rectangles
     for i, d in enumerate(detections):# type(d) == dlib.rectangle
-        print("\t\tBeginning prediction (detection " + str(i) + ")")
-        tstart = datetime.now()
+        print("Beginning prediction for detection " + str(i))
         shape = predictor(img, d)# type(shape) == dlib.full_object_detection
 
         pts = []
         for p in shape.parts():
             pts.append((p.x, p.y))
         ptsList.append(pts)
-	
-	breadthList.append(np.sqrt(d.width() ** 2 + d.height() ** 2)) #this is a list of magnitudes of the hypotenuse (so called breadth) of the face detection
-	tend = datetime.now()
-	print("\t\t" + str(tend - tstart))
+	ptsList.append((d.width(), d.height()))
 
-    return ptsList, breadthList
+    return ptsList
 
 def getMetadata(input_path):
     ffprobe = subprocess.Popen(["ffprobe", "-show_streams", input_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -134,64 +99,47 @@ def getMetadata(input_path):
     return [metaWidth, metaHeight, metaAvgRate, metaFrameNumb]	
 
 def getPupilData(input_path):
-    print("\t\tsubprocessing pupil data")
-    tstart = datetime.now()
     eyeLine = subprocess.Popen(["./eyeLine", "-d", input_path], stdout=subprocess.PIPE)
     eyeLine.wait()
     eyeLineStdout = eyeLine.communicate()[0]
 
     spaceIndexing = eyeLineStdout.split(" ")
+    spaceIndexing = spaceIndexing[:len(spaceIndexing)-1]
+
     pupilData = []
 
-    if len(spaceIndexing) % 5 == 0:				#it parsed appropriately
-    	spaceIndexing = spaceIndexing[:len(spaceIndexing)-1] 	#remove newlines elements from parsing, NOT FINISHED, ONLY HANDLES ONE SET ATM... LAZINESS
-        for i, sp in enumerate(spaceIndexing):
-            if i & 1 == 0: #every other one (think c: step+=2)
-                pupilData.append((int(sp), int(spaceIndexing[i+1])))
-   
-    else:
-        pupilData.append((-1, -1)) #dummy data necessary for drawing appropriate scaled pupils later
-    
-    tend = datetime.now()
-    print("\t\t" + str(tend - tstart))
-    return pupilData
+    for i, pupilCoord in enumerate(spaceIndexing):
+        if i & 1 == 0 and pupilCoord != "Face":
+            print "Is this face and a string?: " + pupilCoord
+            pupilData.append((int(pupilCoord), int(spaceIndexing[i+1])))
+    return pupilData 
 
 def handleFrame(input_path, detector, predictor, wait_at_frame):
-    global lazy_output_file, global_script_start
-    print("\tBase encoding time")
-    tstart = datetime.now()
+    img = cv2.imread(input_path, 1)
+    ptsList = detectFrame(img, detector, predictor)
 
-    if URI_Handling:
-        content_file = open(fn, 'r')
-        img = data_uri_to_cv2_img(content_file.read())
-        content_file.close()
-    else:
-	img = cv2.imread(input_path, 1)
-    tend = datetime.now()
-    print("\t" + str(tend - tstart))
-
-    ptsList, breadthList = detectFrame(img, detector, predictor)
-    markFrame(img, ptsList, breadthList, wait_at_frame)
-    #write ptsList to JSON to DB
-
-    if not realTime:
-        pupilData = getPupilData(input_path) #IPC to get pupil data
-        markPupil(img, pupilData, breadthList, wait_at_frame)
-        #write pupil data to DB
+    #write ptsList to JSON
+    #write JSON to DB
 	
-    cv2.imwrite((lazy_output_file if newCopy else input_path), img) 
-    tend = datetime.now()
-    print("\ttime to write since encoding")
-    print("\t" + str(tend - tstart)) 
+    pupilData = getPupilData(input_path) #IPC to get pupil data
+    if len(pupilData) > 0:
+        for i, pl in enumerate(ptsList):
+            if i & 1 == 0:
+	        ptsList[i].append(pupilData[i/2])
+
+    #write pupil data to DB
+	
+    markFrame(img, ptsList, wait_at_frame)
+    if len(ptsList) >= 1:
+        markPupil(img, [pupilData, ptsList[1]], wait_at_frame)
+    cv2.imwrite(((input_path[:input_path.rfind(".")] + "_final.png") if newCopy else input_path), img) 
     return img #for the sake of the single frame option (needed metadata)
 
 if __name__ == "__main__":
-    global_script_start = datetime.now()
 
     if len(sys.argv) < 2:
         print("Error: Too few arguments supplied.")
         exit()
-
     elif len(sys.argv) > 14:
         print("Error: Too many arguments supplied.")
         exit()
@@ -199,10 +147,9 @@ if __name__ == "__main__":
     run_dir = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
     predictor_path = run_dir + "/shape_predictor_68_face_landmarks.dat"
     newCopy = True
-    realTime = False
     single_frame = False
     start_time = "00:00"
-    duration = "-1"
+    duration = "00:01"
     input_path = ""
     output_path = ""
     verbose = False
@@ -213,12 +160,8 @@ if __name__ == "__main__":
         exit()
 
     for index, arg in enumerate(sys.argv):
-        if arg == "-uri":
-	    URI_Handling = True 
-	elif arg == "-l":
+        if arg == "-l":
             newCopy = False
-	elif arg == "-r":
-	    realTime = True
         elif arg == "-p":
             predictor_path = sys.argv[index+1]
         elif arg == "-f":
@@ -233,8 +176,6 @@ if __name__ == "__main__":
                 output_path = input_path[:input_path.rfind(".")] + "_final.mp4"
         elif arg == "-o":
             output_path = sys.argv[index+1]
-	    lazy_output_file = output_path
-	    print("Output path is now" + lazy_output_file)
         elif arg == "-v":
             verbose = True
         elif arg == "-w":
@@ -244,66 +185,41 @@ if __name__ == "__main__":
         print("Please enter an input file")
         exit()
 
-    print("Calling DLib")
-    tpeter = datetime.now()
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(predictor_path)
-    tgay = datetime.now()
-    print("Finished calling at " + str(tgay - tpeter)) #WTF
 
     outstream = (None if verbose else open(os.devnull, 'w')) #for redirecting output to nothing as desired
 
     if single_frame:
-	print("Handling a frame")
-	tstart = datetime.now()
 	img = handleFrame(input_path, detector, predictor, wait_at_frame)
-        print("Image " + input_path + ": Width - " + str(img.shape[0]) + ", Height - " + str(img.shape[1])) #write to DB?
-	tend = datetime.now()
-	print("Finished handling frame at" + str(tend - tstart))
+        print("Image " + input_path + ": Width - " + str(img.shape[0]) + ", Height - " + str(img.shape[1]))
 
 
     else:
-	("Gathering metadata")
-	tstart = datetime.now()
-	
 	metadata = getMetadata(input_path)
 	for d in metadata:
 	    print(d)
 	#write metadata to DB
-
-	tend = datetime.now()
-	print("Finished gathering at " + str(tend - tstart))
-
+        
         midPath = input_path[:input_path.rfind(".")] + "%d.png"
-	print("Breaking into frames")
-	tstart = datetime.now()
-        ffmpegBreak = subprocess.Popen(["ffmpeg", "-ss", start_time] + ["-t", duration] + ["-i", input_path, midPath], stdout=outstream, stderr=subprocess.STDOUT) # Run FFMPEG, using pngs
+        ffmpegBreak = subprocess.Popen(["ffmpeg", "-ss", start_time, "-t", duration, "-i", input_path, midPath], stdout=outstream, stderr=subprocess.STDOUT) # Run FFMPEG, using pngs
 	ffmpegBreak.wait()
-	tend = datetime.now()
-	print("Finished breaking at " + str(tend - tstart))
 
         g = glob.glob(input_path[:input_path.rfind(".")] + "*.png")
         for i, fn in enumerate(g):
-                print("Handling frame " + str(i) + ":")
-		tstart = datetime.now()
+                print("Image " + str(i) + ":")
 		handleFrame(fn, detector, predictor, wait_at_frame)
-		tend = datetime.now()
-		print("Finished handling frame " + str(tend - tstart))
 
         if newCopy:
             midPath = midPath[:midPath.rfind(".")] + "_final.png"
         
-	print("Rebuilding video")
-	tstart = datetime.now()
 	ffmpegBuild = subprocess.Popen(["ffmpeg", "-i", midPath, output_path], stdout=outstream, stderr=subprocess.STDOUT)# Run FFMPEG to rebake the video
         ffmpegBuild.wait()
-	tend = datetime.now()
-	print("Finished rebuilding at " + str(tend - tstart))
+
 	if newCopy:
             for f in glob.glob(input_path[:input_path.rfind(".")] + "*_final.png"):
                 os.remove(f)
         else:
             for f in glob.glob(input_path[:input_path.rfind(".")] + "*.png"):
                 os.remove(f)
-    end = datetime.now()
-    print("Execution Complete at " + str(end - global_script_start))
+    print("Execution Complete.")
