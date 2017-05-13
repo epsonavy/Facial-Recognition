d00f38b0333c4818ed00bd90284a25a8bc3daea8
root <root@ip-172-31-17-236.us-west-1.compute.internal>
2017-05-13 03:02:17 +0000

    fixed upload button issue

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $box      = $('#upload-box');
     $progress = $('#progress');
     $list     = $('#list');
-	$list_m   = $('#list_mobile');
+//	$list_m   = $('#list_mobile');
 
     $video.attr({
         controls : true,
